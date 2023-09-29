3b0d8fe2c1308aff99ab060c0dae7af05012bfa4
CarlosRR19 <carlosrr19@educastur.es>
2023-09-29 13:47:13 +0200

    update

--- ejercicio4/ejercicio4.js
+++ ejercicio4/ejercicio4.js
@@ -1,4 +1,75 @@
+const OPERADORES = ["+", "-", "*", "/", "FIN"];
 var operador = prompt("Introduce un operador");
-var operando = prompt("Introduce un operando");
-var operación = prompt("Introduce el símbolo de la operación que deseas realizar")
 
+function validarOperador(operador) {
+  var valido = false;
+
+  for (var i = 0; i < OPERADORES.length; i++) {
+    if (operador == OPERADORES[i]) {
+      valido = true;
+      break;
+    }
+  }
+
+  return valido;
+}
+
+function divisonValida(dividendo, divisor) {
+  var valido = false;
+  if (divisor != 0) {
+    valido = true;
+  }
+
+  return valido;
+}
+
+function calcular(operador) {
+  do {
+    do {
+      validarOperador(operador);
+
+      if (validarOperador(operador) == false) {
+        operador = prompt("Operador inválido");
+      }
+    } while (validarOperador(operador) == false);
+
+    if (operador == "FIN") {
+      break;
+    }
+
+    var operandoP = parseInt(prompt("Introduce un operando"));
+    var operandoS = parseInt(prompt("Introduce un operando"));
+
+    switch (operador) {
+      case "+":
+        console.log(operandoS + operandoP);
+        operador = prompt("Introduce un operador");
+        break;
+
+      case "-":
+        console.log(operandoP - operandoS);
+        operador = prompt("Introduce un operador");
+        break;
+
+      case "*":
+        console.log(operandoP * operandoS);
+        operador = prompt("Introduce un operador");
+        break;
+
+      case "/":
+        do {
+          if (divisonValida(operandoP, operandoS) == false) {
+            operandoS = prompt("Divisor inválido");
+          }
+        } while (divisonValida(operandoP, operandoS) == false);
+        console.log(operandoP / operandoS);
+        operador = prompt("Introduce un operador");
+        break;
+
+      case "FIN":
+        break;
+    }
+  } while (operador != "FIN");
+}
+
+calcular(operador);
---
+++ ejercicio5/ejercicio5.js
@@ -0,0 +1,50 @@
+const USUARIOS = ["pepe", "paco", "juan", "benito", "manuel"];
+const CONTRASENAS = ["12345", "23456", "09876", "67890", "66666"];
+
+function existeLogin(login) {
+  var valido = false;
+  for (var i = 0; i < USUARIOS.length; i++) {
+    if (login == USUARIOS[i]) {
+      valido = true;
+      break;
+    }
+  }
+
+  return valido;
+}
+
+function passwordCorrecto(password, login) {
+  var valido = false;
+
+  for (var i = 0; 0 < USUARIOS.length; i++) {
+    if (login == USUARIOS[i]) {
+      if (password == CONTRASENAS[i]) {
+        valido = true;
+      }
+      break;
+    }
+  }
+
+  return valido;
+}
+
+function iniciarSesion() {
+  for (var i = 0; i < 5; i++) {
+    var login = prompt("Nombre de usuario");
+    var password = prompt("Contraseña");
+
+    if (passwordCorrecto(password, login) == true) {
+      window.alert("El nombre de usuario y la contraseña son correctos");
+      break;
+    } else if (existeLogin(login) == false) {
+      window.alert("El nombre de usuario no existe");
+    } else if (
+      passwordCorrecto(login, password == false) &&
+      existeLogin(login) == true
+    ) {
+      window.alert("El nombre de usuario no coincide con la contraseña");
+    }
+  }
+}
+
+iniciarSesion();
